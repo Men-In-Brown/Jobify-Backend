d0ec4b7a0d49977bbcbdc27009ff73c374fa7592
Shaurya Goel <shauryaggamer@gmail.com>
2024-03-20 12:47:36 -0700

    fixing imports for hallpass.java

--- src/main/java/com/nighthawk/spring_portfolio/mvc/hallpass/HallPass.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/hallpass/HallPass.java
@@ -1,9 +1,9 @@
 package com.nighthawk.spring_portfolio.mvc.hallpass;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
 import java.time.LocalDateTime;
 
 @Entity
