ebf5fefc283d74d9c5fcd06c3190835e2070bf76
BobTheFarmer <colinmweis@gmail.com>
2024-04-18 08:46:49 -0700

    Quiz and grading

--- src/main/java/com/nighthawk/spring_portfolio/mvc/assignment/AssignmentApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/assignment/AssignmentApiController.java
@@ -10,7 +10,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
@@ -24,26 +24,32 @@ public class AssignmentApiController {
 
     // Autowired enables Control to connect URI request and POJO Object to easily for Database CRUD operations
     @Autowired
-    private AssignmentJpaRepository repository;
+    private AssignmentJpaRepository assignmentRepository;
+
+    @Autowired
+    private QuizJpaRepository quizRepository;
 
     @Autowired
     private GradeJpaRepository gradeRepository;
 
 
-    /* GET List of Jokes
-     * @GetMapping annotation is used for mapping HTTP GET requests onto specific handler methods.
-     */
+    int idCount = 1; //Needed to prevent id overlap with quizzes and assignments
+
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping("/")
-    public ResponseEntity<List<Assignment>> getJokes() {
-        // ResponseEntity returns List of Jokes provide by JPA findAll()
-        return new ResponseEntity<>( repository.findAll(), HttpStatus.OK);
+    public ResponseEntity<List<Object>> getRepositories() {
+        List<Object> combined = new ArrayList<>();
+        combined.addAll(assignmentRepository.findAll());
+        combined.addAll(quizRepository.findAll());
+        return new ResponseEntity<>(combined, HttpStatus.OK);
     }
+
+    
 
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping("/{id}")
     public ResponseEntity<Assignment> getById(@PathVariable Long id) {
-        Optional<Assignment> assignmentOptional = repository.findById(id);
+        Optional<Assignment> assignmentOptional = assignmentRepository.findById(id);
     
         return assignmentOptional.map(assignment -> new ResponseEntity<>(assignment, HttpStatus.OK))
                 .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
@@ -69,13 +75,71 @@ public class AssignmentApiController {
             return new ResponseEntity<>("maxPoints must be positive", HttpStatus.BAD_REQUEST);
         }
         
-        repository.save(new Assignment(title, desc, link, maxPoints));
+        assignmentRepository.save(new Assignment(title, desc, link, maxPoints, idCount));
+        idCount++;
         return new ResponseEntity<>("Created successfully", HttpStatus.CREATED);
     }
-    /* Update Like
-     * @PutMapping annotation is used for mapping HTTP PUT requests onto specific handler methods.
-     * @PathVariable annotation extracts the templated part {id}, from the URI
-     */
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PostMapping("/postQuiz")
+    public ResponseEntity<Object> postQuiz(
+    @RequestParam("title") String title,
+    @RequestParam("desc") String desc,
+    @RequestParam("maxPoints") int maxPoints,
+    @RequestBody final Map<String,Object> questions) {
+        if(title.length() < 3 || title.length() > 100) {
+            return new ResponseEntity<>("Title is less than 3 or longer than 100 characters", HttpStatus.BAD_REQUEST);
+        }
+        if(desc.length() < 3 || desc.length() > 50000) {
+            return new ResponseEntity<>("Desc is less than 3 or longer than 500000 characters", HttpStatus.BAD_REQUEST);
+        }
+        if(maxPoints <= 0) {
+            return new ResponseEntity<>("maxPoints must be positive", HttpStatus.BAD_REQUEST);
+        }
+        
+        //Check Questions
+        // Extract Attributes from JSON
+
+        Map<String, Object> attributeMap = new HashMap<>();
+        int questionNum = 1;
+        for (Map.Entry<String,Object> questionTemp : questions.entrySet())  {
+            if (!(questionTemp.getValue() instanceof HashMap)) {
+                return new ResponseEntity<>("Questions should contain hashmaps", HttpStatus.BAD_REQUEST);
+            }
+            Map<String, Object> question = ((Map<String, Object>)questionTemp.getValue());
+
+            Map<String, Object> subAttributeMap = new HashMap<>();
+            
+
+            for (Map.Entry<String,Object> attribute : question.entrySet())  {
+                if(attribute.getKey().equals("answers"))
+                if (attribute.getValue() instanceof List) {
+                    subAttributeMap.put(attribute.getKey(), attribute.getValue());
+                } else {
+                    return new ResponseEntity<>("Answers attribute should be a list", HttpStatus.BAD_REQUEST);
+                }
+            
+                if(attribute.getKey().equals("correctAnswer"))
+                    subAttributeMap.put(attribute.getKey(), attribute.getValue());
+
+                if(attribute.getKey().equals("desc"))
+                    subAttributeMap.put(attribute.getKey(), attribute.getValue());
+                
+            }
+            //Does it have all attributes?
+            if(!(subAttributeMap.containsKey("answers")  && subAttributeMap.containsKey("correctAnswer") && subAttributeMap.containsKey("desc"))) {
+                return new ResponseEntity<>("Missing attributes for question #" + questionNum + "answers, correctAnswer, and desc are required: " + subAttributeMap, HttpStatus.BAD_REQUEST); 
+            }   
+
+            //Add question data to main attribute map
+            attributeMap.put(Integer.toString(questionNum), subAttributeMap);
+            questionNum++;
+        }
+        
+        quizRepository.save(new Quiz(title, desc, maxPoints, idCount, attributeMap));
+        idCount++;
+        return new ResponseEntity<>("Created successfully", HttpStatus.CREATED);
+    }
 
     @PostMapping(value = "/submit", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Object> personStats(@RequestBody final Map<String,Object> request_map) {
@@ -83,8 +147,11 @@ public class AssignmentApiController {
         if (!(request_map.get("id") instanceof String)){
             return new ResponseEntity<>("id should be a String", HttpStatus.BAD_REQUEST);
         }
-        long id=Long.parseLong((String)request_map.get("id"));  
-        Optional<Assignment> optional = repository.findById((id));
+        int id=Integer.parseInt((String)request_map.get("id"));  
+
+        List<Assignment> assignments = assignmentRepository.findByJointId(id);
+        Optional<Assignment> optional = assignments.isEmpty() ? Optional.empty() : Optional.of(assignments.get(0));
+
         if (optional.isPresent()) {  // Good ID
             Assignment assignment = optional.get();  // value from findByID
 
@@ -93,35 +160,25 @@ public class AssignmentApiController {
             for (Map.Entry<String,Object> submission : request_map.entrySet())  {
                 // Add needed attributes to attributeMap
 
-                if(submission.getKey().equals("contributors"))
+                if(submission.getKey().equals("answers"))
                     if (submission.getValue() instanceof List) {
                         attributeMap.put(submission.getKey(), submission.getValue());
                     } else {
                         return new ResponseEntity<>("Contributors attribute should be a list", HttpStatus.BAD_REQUEST);
                     }
-                
-                if(submission.getKey().equals("title"))
-                    attributeMap.put(submission.getKey(), submission.getValue());
-                
-                if(submission.getKey().equals("desc"))
-                    attributeMap.put(submission.getKey(), submission.getValue());
-
-                if(submission.getKey().equals("link"))
-                    attributeMap.put(submission.getKey(), submission.getValue());
-                    
             }
 
             //Does it have all attributes?
-            if(!(attributeMap.containsKey("contributors")  && attributeMap.containsKey("title") && attributeMap.containsKey("desc") && attributeMap.containsKey("link"))) {
-                return new ResponseEntity<>("Missing attributes. username, contributors, title, desc, and link are required" + attributeMap, HttpStatus.BAD_REQUEST); 
+            if(!(attributeMap.containsKey("answers")  && attributeMap.containsKey("username"))) {
+                return new ResponseEntity<>("Missing attributes. username and answers are required" + attributeMap, HttpStatus.BAD_REQUEST); 
             }
 
             // Set Date and Attributes to SQL HashMap
             Map<String, Map<String, Object>> date_map = assignment.getSubmissions();
-            date_map.put( (String) request_map.get("username"), attributeMap );
+            date_map.put( (String) request_map.get("username"), attributeMap);
 
             assignment.setSubmissions(date_map);
-            repository.save(assignment);
+            assignmentRepository.save(assignment);
 
             //Save grade as well for each contributor
             List<String> contributors = (List<String>) attributeMap.get("contributors");
@@ -135,6 +192,65 @@ public class AssignmentApiController {
             // return Person with update Stats
             return new ResponseEntity<>(assignment, HttpStatus.OK);
         }
+
+        //Check for quiz
+
+        List<Quiz> quizzes = quizRepository.findByJointId(id);
+        Optional<Quiz> optional2 = quizzes.isEmpty() ? Optional.empty() : Optional.of(quizzes.get(0));
+
+        if (optional2.isPresent()) {  // Good ID
+            Quiz quiz = optional2.get();  // value from findByID
+
+            // Extract Attributes from JSON
+            Map<String, Object> attributeMap = new HashMap<>();
+            for (Map.Entry<String,Object> submission : request_map.entrySet())  {
+                // Add needed attributes to attributeMap
+
+                if(submission.getKey().equals("answers"))
+                    if (submission.getValue() instanceof List) {
+                        attributeMap.put(submission.getKey(), submission.getValue());
+                    } else {
+                        return new ResponseEntity<>("Contributors attribute should be a list", HttpStatus.BAD_REQUEST);
+                    }
+                
+                if(submission.getKey().equals("username"))
+                    attributeMap.put(submission.getKey(), submission.getValue());
+            }
+
+            //Does it have all attributes?
+            if(!(attributeMap.containsKey("answers")  && attributeMap.containsKey("username"))) {
+                return new ResponseEntity<>("Missing attributes. username and answers are required" + attributeMap, HttpStatus.BAD_REQUEST); 
+            }
+
+            // Set Date and Attributes to SQL HashMap
+            Map<String, Map<String, Object>> date_map = quiz.getSubmissions();
+            date_map.put( (String) request_map.get("username"), attributeMap );
+
+            quiz.setSubmissions(date_map);
+            quizRepository.save(quiz);
+
+            //Grade it
+            ArrayList<Integer> submittedAnswers = (ArrayList<Integer>)attributeMap.get("answers");
+            Map<String, Object> questions = quizRepository.getById(Long.parseLong(Integer.toString(id))).getQuestions();
+            
+            int score = 0;
+            int i = 0;
+            for(Map.Entry<String,Object> question : questions.entrySet()) {
+                if(((Map<String, Object>)question.getValue()).get("correctAnswer") == submittedAnswers.get(i)) {
+                    score++;
+                } else {
+
+                }
+                i++;
+            }
+
+            Grade grade = new Grade((String)attributeMap.get("username"), "temp", quiz.getTitle(), quiz.getMaxPoints(), score);
+            gradeRepository.save(grade);
+
+            // return Person with update Stats
+            return new ResponseEntity<>(quiz, HttpStatus.OK);
+        }
+
         // return Bad ID
         return new ResponseEntity<>("Bad ID", HttpStatus.BAD_REQUEST); 
     }
